83a4d68fdc46f449dbe0b30c768c7da71abdae94
Balaji K <balajibala76394@gmail.com>
2023-03-03 04:26:47 +0530

    backend indexing a name

--- backend/schemas/index.js
+++ backend/schemas/index.js
@@ -28,6 +28,65 @@ import volumeDry from './common/volume-dry'
 import volume from './common/volume'
 import WeightandMass from './common/WeightandMass'
 import commonUnitSystem from './commonUnitSystem/common'
+import charge from './electricity/Charge'
+import currencyConverter from './electricity/CurrentConverter'
+import ElectricConductanceConverter from './electricity/ElectricConductanceConverter'
+import ElectricConductivity from './electricity/ElectricConductivity'
+import ElectricFieldStrength from './electricity/ElectricFieldStrength'
+import ElectricPotential from './electricity/ElectricPotential'
+import ElectricResistance from './electricity/ElectricResistance'
+import ElectrostaticCapacitance from './electricity/ElectrostaticCapacitance'
+import Inductance from './electricity/Inductance'
+import LinearChargeDensity from './electricity/LinearChargeDensity'
+import LinearCurrentDensity from './electricity/LinearCurrentDensity'
+import SurfaceChargeDensity from './electricity/SurfaceChargeDensity'
+import SurfaceCurrentDensity from './electricity/SurfaceCurrentDensity'
+import VolumeChargeDensity from './electricity/VolumeChargeDensity'
+import Acceleration from './engineering/Acceleration'
+import AccelerationAngular from './engineering/AccelerationAngular'
+import Density from './engineering/Density'
+import MomentForce from './engineering/MomentForce'
+import MomentofInertia from './engineering/MomentofInertia'
+import Specific from './engineering/Specific'
+import Torque from './engineering/Torque'
+import Velocity from './engineering/Velocity'
+import concentrationMolar from './fluids/ConcentrationMolar'
+import ConcentrationSolution from './fluids/ConcentrationSolution'
+import flow from './fluids/flow'
+import FlowMass from './fluids/FlowMass'
+import FlowMolar from './fluids/FlowMolar'
+import MassFluxDensity from './fluids/MassFluxDensity'
+import Permeability from './fluids/Permeability'
+import SurfaceTension from './fluids/SurfaceTension'
+import ViscosityDynamic from './fluids/ViscosityDynamic'
+import ViscosityKinematic from './fluids/ViscosityKinematic' 
+import fuelEfficiencyVolume from './heat/FuelEfficiencyVolume'
+import FuelEfficientmass from './heat/FuelEfficientmass'
+import HeatDensity from './heat/HeatDensity'
+import HeatFluxDensity from './heat/HeatFluxDensity'
+import HeatTransferCoefficient from './heat/HeatTransferCoefficient'
+import SpecificHeatCapacity from './heat/SpecificHeatCapacity'
+import TemperatureInterval from './heat/TemperatureInterval'
+import ThermalConductivity from './heat/ThermalConductivity'
+import ThermalExpansion from './heat/ThermalExpansion'
+import ThermalResistance from './heat/ThermalResistance'
+import DigitalImageResolution from './light/DigitalImageResolution'
+import FrequencyWavelength from './light/FrequencyWavelength'
+import Illumination from './light/Illumination'
+import Luminance from './light/Luminance'
+import LuminousIntensity from './light/LuminousIntensity'
+import MagneticFieldStrength from './magnetism/MagneticFieldStrength'
+import MagneticFlux from './magnetism/MagneticFlux'
+import MagneticFluxDensity from './magnetism/MagneticFluxDensity'
+import MagnetomotiveForce from './magnetism/MagnetomotiveForce'
+import commonUnitSystem1 from './others/CommonUnitSystems'
+import Radiation from './radiology/Radiation'
+import RadiationAbsorbedDose from './radiology/RadiationAbsorbedDose'
+import RadiationActivity from './radiology/RadiationActivity'
+import RadiationExposure from './radiology/RadiationExposure'
+
+
+
 
 export const schemaTypes = [
   allunit,
@@ -60,4 +119,61 @@ export const schemaTypes = [
   volume,
   WeightandMass,
   commonUnitSystem,
+  charge,
+  currencyConverter,
+  ElectricConductanceConverter,
+  ElectricConductivity,
+  ElectricFieldStrength,
+  ElectricPotential,
+  ElectricResistance,
+  ElectrostaticCapacitance,
+  Inductance,
+  LinearChargeDensity,
+  LinearCurrentDensity,
+  SurfaceChargeDensity,
+  SurfaceCurrentDensity,
+  VolumeChargeDensity,
+  Acceleration,
+  AccelerationAngular,
+  Density,
+  MomentForce,
+  MomentofInertia,
+  Specific,
+  Torque,
+  Velocity,
+  concentrationMolar,
+  ConcentrationSolution,
+  flow,
+  FlowMass,
+  FlowMolar,
+  MassFluxDensity,
+  Permeability,
+  SurfaceTension,
+  ViscosityDynamic,
+  ViscosityKinematic,
+  fuelEfficiencyVolume,
+  FuelEfficientmass,
+  HeatDensity,
+  HeatFluxDensity,
+  HeatTransferCoefficient,
+  SpecificHeatCapacity,
+  TemperatureInterval,
+  ThermalConductivity,
+  ThermalExpansion,
+  ThermalResistance,
+  DigitalImageResolution,
+  FrequencyWavelength,
+  Illumination,
+  Luminance,
+  LuminousIntensity,
+  MagneticFieldStrength,
+  MagneticFlux,
+  MagneticFluxDensity,
+  MagnetomotiveForce,
+  commonUnitSystem1,
+  Radiation,
+  RadiationAbsorbedDose,
+  RadiationActivity,
+  RadiationExposure
+
 ]
